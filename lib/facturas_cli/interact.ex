82d4d660f61bd7ff32ddfc4a958430ccd4e54e0b
ismqui <ismqui@gmail.com>
2019-06-28 23:41:28 +0200

    add more functions

--- lib/facturas_cli/interact.ex
+++ lib/facturas_cli/interact.ex
@@ -10,5 +10,27 @@ defmodule FacturasCli.Interact do
     IO.puts("#{dir_fact} #{fichero_fact}")
     Facturas.FacturasFile.load("#{dir_fact}/#{fichero_fact}")
     |> IO.inspect()
+
+    case confirm("Continuar?[Y/n]") do
+      true ->
+        IO.ANSI.clear()
+        start()
+      false ->
+        IO.puts "Finalizando..."
+    end
+
   end
+
+  def confirm(question) do
+    IO.ANSI.clear_line()
+    answer = question |> IO.gets() |> String.trim() |> String.downcase()
+
+    case answer do
+      "y" -> true
+      "n" -> false
+      _ -> confirm(question)
+    end
+  end
+
+
 end
